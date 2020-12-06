9b2d4e2f6609daee2959a499f0891425be5cadad
Rosano Coutinho <pub@xeari.com>
2020-12-06 10:07:12 -0300

    Spec OLSKControllerGlobalMiddleware

--- os-app/tech-remit/controller-tests.js
+++ os-app/tech-remit/controller-tests.js
@@ -1,11 +1,104 @@
-const { deepEqual } = require('assert');
+const { throws, deepEqual } = require('assert');
 
 const mod = require('./controller.js');
+
+describe('OLSKControllerGlobalMiddleware', function test_OLSKControllerGlobalMiddleware() {
+
+	const _OLSKControllerGlobalMiddleware = function (params) {
+		return Object.assign(Object.assign({}, mod), {
+			DataResponse: (function () {}),
+		}, params).OLSKControllerGlobalMiddleware(Object.assign({
+			hostname: Math.random().toString(),
+			path: Math.random().toString(),
+		}, params), Object.assign({
+			send: (function () {
+				return [].concat(...arguments);
+			}),
+		}, params), params.next || function () {});
+	};
+
+	it('calls DataResponse if match', async function () {
+		const hostname = uRandomElement(Object.keys(mod.DataDomainMap()));
+		const path = Math.random().toString();
+
+		deepEqual(await _OLSKControllerGlobalMiddleware({
+			hostname,
+			path,
+			DataResponse: (function () {
+				return Array.from(arguments);
+			}),
+		}), [mod.DataDomainMap()[hostname], path]);
+	});
+
+	it('calls next', async function () {
+		const next = Math.random().toString();
+
+		deepEqual(await _OLSKControllerGlobalMiddleware({
+			hostname: Math.random().toString(),
+			next: (function () {
+				return next;
+			}),
+		}), next);
+	});
+
+	it('returns res.send', async function () {
+		const DataResponse = Math.random().toString();
+
+		deepEqual(await _OLSKControllerGlobalMiddleware({
+			hostname: uRandomElement(Object.keys(mod.DataDomainMap())),
+			send: (function () {
+				return Array.from(arguments);
+			}),
+			DataResponse: (function () {
+				return Promise.resolve(DataResponse);
+			}),
+		}), [DataResponse]);
+	});
+
+});
 
 describe('DataDomainMap', function test_DataDomainMap() {
 
 	it('returns array', function () {
 		deepEqual(mod.DataDomainMap(), JSON.parse(process.env.GRD_REMIT_MAP));
+	});
+
+});
+
+describe('DataResponse', function test_DataResponse() {
+
+	const _DataResponse = function (params) {
+		return Object.assign(Object.assign({}, mod), {
+			_DataContent: (function () {}),
+		}, params).DataResponse(params.root || Math.random().toString(), params.path || Math.random().toString(), params.callback || function () {});
+	};
+
+	it('calls _DataContent', function () {
+		const root = Math.random().toString();
+		const path = Math.random().toString();
+
+		deepEqual(_DataResponse({
+			root,
+			path,
+			_DataContent: (function () {
+				return Array.from(arguments);
+			}),
+		}), [root + path]);
+	});
+
+	it('replaces /', function () {
+		const root = Math.random().toString();
+
+		deepEqual(_DataResponse({
+			root,
+			path: '/',
+			_DataContent: (function () {
+				return Array.from(arguments);
+			}),
+			callback: (function () {
+				return Array.from(arguments);
+			}),
+		}), [root + '/index.html']);
 	});
 
 });
--- os-app/tech-remit/controller.js
+++ os-app/tech-remit/controller.js
@@ -1,9 +1,48 @@
+// How to get node.js HTTP request promise without a single dependency https://www.tomas-dvorak.cz/posts/nodejs-request-without-dependencies/
+const uGet = function (inputData) {
+  return new Promise((resolve, reject) => {
+    (inputData.startsWith('https') ? require('https') : require('http')).get(inputData, (response) => {
+      if (response.statusCode < 200 || response.statusCode > 299) {
+      	return reject(new Error('Error: ' + response.statusCode));
+      }
+
+      const body = [];
+
+      response.on('data', function (chunk) {
+      	return body.push(chunk);
+      });
+
+      response.on('end', function () {
+      	return resolve(body.join(''))
+      });
+    }).on('error', reject);
+  });
+};
+
 const mod = {
+
+	async OLSKControllerGlobalMiddleware (req, res, next) {
+		if (!this.DataResponse) {
+			Object.assign(this, mod); // #hotfix-oldskool-middleware-this
+		}
+
+		if (!mod.DataDomainMap()[req.hostname]) {
+			return next();
+		}
+		
+		return res.send(await this.DataResponse(mod.DataDomainMap()[req.hostname], req.path));
+	},
 
 	// DATA
 
 	DataDomainMap() {
 		return JSON.parse(process.env.GRD_REMIT_MAP);
+	},
+
+	_DataContent: uGet,
+
+	DataResponse (root, path) {
+		return this._DataContent(root + (path === '/' ? '/index.html' : path));
 	},
 
 };
