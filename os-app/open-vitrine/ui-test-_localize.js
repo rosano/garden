91b5f757b76bd81d8e5f34a54f3319c66616015a
Rosano Coutinho <pub@xeari.com>
2021-04-28 10:25:04 -0300

    Tidy tests

--- os-app/open-vitrine/ui-test-_localize.js
+++ os-app/open-vitrine/ui-test-_localize.js
@@ -6,7 +6,7 @@ kDefaultRoute.OLSKRouteLanguageCodes.forEach(function (OLSKRoutingLanguage) {
 		return OLSKTestingLocalized(inputData, OLSKRoutingLanguage);
 	};
 
-	describe(`GRDVitrine_Localize-${ OLSKRoutingLanguage }`, function () {
+	describe('GRDVitrine_Localize-' + OLSKRoutingLanguage, function () {
 
 		before(function() {
 			return browser.OLSKVisit(kDefaultRoute, {
